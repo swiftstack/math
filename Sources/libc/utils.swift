14093478e2ff09c1d464e5b0fc2bf5844ab9e3a1
Tony Freeman <tonyfreeman@users.noreply.github.com>
2020-03-29 12:44:35 +0000

    [libc] Add setters for highWord and lowWord

--- Sources/libc/utils.swift
+++ Sources/libc/utils.swift
@@ -4,18 +4,22 @@ extension Double {
         self.init(bitPattern: UInt64(high) << 32 | UInt64(low))
     }
 
-    @inline(__always)
-    mutating func clearLowWord() {
-        self = Double(bitPattern: bitPattern & 0xffffffff_00000000)
-    }
-
     var highWord: UInt32 {
         @inline(__always)
         get { return UInt32(truncatingIfNeeded: bitPattern >> 32) }
+        @inline(__always)
+        set { self = .init(high: newValue, low: lowWord) }
     }
 
     var lowWord: UInt32 {
         @inline(__always)
         get { return UInt32(truncatingIfNeeded: bitPattern) }
+        @inline(__always)
+        set { self = .init(high: highWord, low: newValue) }
+    }
+
+    @inline(__always)
+    mutating func clearLowWord() {
+        self = Double(bitPattern: bitPattern & 0xffffffff_00000000)
     }
 }
