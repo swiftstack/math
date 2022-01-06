dc6bb08dcb45f38e4f77abcbddc2f1efc250b62d
Tony Freeman <tonyfreeman@users.noreply.github.com>
2022-01-06 08:46:20 +0300

    [Tests] Reflect API changes

--- Tests/LibcTests/LibcTests.swift
+++ Tests/LibcTests/LibcTests.swift
@@ -4,17 +4,17 @@ import Test
 final class LibcTests: TestCase {
     func testLog2() {
         #if !arch(x86_64)
-        assertEqual(log2(42.0), 5.3923174227787607)
+        expect(log2(42.0) == 5.3923174227787607)
         #endif
     }
 
     func testScalbn() {
-        assertEqual(scalbn(0.7937005259840998, -1), 0.3968502629920499)
+        expect(scalbn(0.7937005259840998, -1) == 0.3968502629920499)
     }
 
     func testExp2() {
-        assertEqual(exp2(3.0), 8.0)
-        assertEqual(exp2(42.0), 4398046511104.0)
-        assertEqual(exp2(-1.3333333333333333), 0.3968502629920499)
+        expect(exp2(3.0) == 8.0)
+        expect(exp2(42.0) == 4398046511104.0)
+        expect(exp2(-1.3333333333333333) == 0.3968502629920499)
     }
 }
--- Tests/MathTests/MathTests.swift
+++ Tests/MathTests/MathTests.swift
@@ -3,6 +3,6 @@ import Test
 
 final class MathTests: TestCase {
     func testLog2() {
-        assertEqual(log2(42.0), 5.3923174227787607)
+        expect(log2(42.0) == 5.3923174227787607)
     }
 }
